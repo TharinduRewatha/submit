4db7e2678d7d1cf145af88fc4c84840c8825e2f3
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 <nmrtharindu@gmail.com>
2023-04-01 16:19:16 +0530

    sff

--- config/db.config.js
+++ config/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     url: "mongodb+srv://nmrtharindu:0000@distributed.wmnq5vl.mongodb.net/test",
-    posttosql:"http://localhost:3003/data/"
+    posttosql:"http://deliverjokes.centralindia.cloudapp.azure.com:3003/data/"
 }
